4c836c24cc9d0e00fa16270234d5e1d3b764d14a
Anupama jha <Anu@gmail.com>
2024-07-24 11:09:38 +0530

    first commit

---
+++ gym/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        The Pain You Feel Today Will Be The Strength You Need Tomorrow
+    </footer>
+  )
+}
+
+export default Footer
